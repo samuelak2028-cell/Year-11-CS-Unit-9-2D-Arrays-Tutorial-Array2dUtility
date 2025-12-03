a045349b800084d025a3207b317754d7a5b437a2
samuel <samuelak2028@student.cis.edu.hk>
2025-12-03 18:42:52 +0800

    Completed all tests.

--- src/Array2dUtility.java
+++ src/Array2dUtility.java
@@ -1,6 +1,109 @@
 
 public class Array2dUtility {
+    public static void print (int[][] array) {
+        for (int i = 0; i<array.length; i++) {
+            for (int j = 0; j<array[i].length; j++) {
+                System.out.println(array[i][j]);
+            }
+        }
+    }
 
+    public static int sum (int[][] array) {
+        int sum = 0;
+        for (int i = 0; i<array.length; i++) {
+            for (int j = 0; j<array[i].length; j++) {
+                sum += array[i][j];
+            }
+        }
+        return sum;
+    }
+
+    public static double average (int[][] array) {
+        return ((double)sum(array)/(array.length*array.length));
+    }
+
+    public static int minimum (int[][] array) {
+        int minimum = array[0][0];
+        for (int i = 0; i<array.length; i++) {
+            for (int j = 0; j<array[i].length; j++) {
+                if (array[i][j] <= minimum) {
+                    minimum = array[i][j];
+                }
+            }
+        }
+        return minimum;
+    }
+
+    public static int maximum (int[][] array) {
+        int maximum = array[0][0];
+        for (int i = 0; i<array.length; i++) {
+            for (int j = 0; j<array[i].length; j++) {
+                if (array[i][j] >= maximum) {
+                    maximum = array[i][j];
+                }
+            }
+        }
+        return maximum;
+    }
+
+    public static int evenCountStandard (int[][] array) {
+        int evens = 0;
+        for (int i = 0; i<array.length; i++) {
+            for (int j = 0; j<array[i].length; j++) {
+                if (array[i][j] % 2 == 0) {
+                    evens++;
+                }
+            }
+        }
+        return evens;
+    }
+
+    public static int evenCountEnhanced (int[][] array) {
+        int evens = 0;
+        for (int[] row : array) {
+            for (int element : row) {
+                if (element % 2 == 0) {
+                    evens++;
+                }
+            }
+        }
+        return evens;
+    }
+
+    public static boolean allPositive (int[][] array) {
+        for (int i = 0; i<array.length; i++) {
+            for (int j = 0; j<array[i].length; j++){
+                if (array[i][j] < 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static int[] rowSums (int[][] array) {
+        int[] result = new int[array.length];
+        for (int i = 0; i<array.length; i++) {
+            int sum = 0;
+            for (int j = 0; j<array[i].length; j++){
+                sum += array[i][j];
+            }
+            result [i] = sum;
+        }
+        return result;
+    }
+
+    public static int[] colSums (int[][] array) {
+        int[] result = new int[array[0].length];
+        for (int i = 0; i<array[0].length; i++) {
+            int sum = 0;
+            for (int j = 0; j<array.length; j++){
+                sum += array[j][i];
+            }
+            result [i] = sum;
+        }
+        return result;
+    }
     // Declare all methods as static.
 
     // 1. print
